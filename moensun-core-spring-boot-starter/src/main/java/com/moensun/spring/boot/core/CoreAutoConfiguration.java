7800944060ce8f44f60531f440100852dc08560b
bane.shi <fengxiaotx@163.com>
2021-12-25 13:38:12 +0800

    act event

--- moensun-core-spring-boot-starter/src/main/java/com/moensun/spring/boot/core/CoreAutoConfiguration.java
+++ moensun-core-spring-boot-starter/src/main/java/com/moensun/spring/boot/core/CoreAutoConfiguration.java
@@ -1,13 +1,22 @@
 package com.moensun.spring.boot.core;
 
+import com.moensun.commons.core.spring.event.act.AbsActEventHandler;
 import com.moensun.commons.core.spring.event.act.ActEventAspect;
+import com.moensun.commons.core.spring.event.act.ActEventHandlerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Configuration(value = "com.moensun.spring.boot.core.CoreAutoConfiguration")
 public class CoreAutoConfiguration {
+
+    @Bean
+    public ActEventHandlerFactory actEventHandlerFactory(List<AbsActEventHandler> actEventHandlers){
+        return new ActEventHandlerFactory(actEventHandlers);
+    }
 
     @Bean
     public ActEventAspect actEventAspect(){
