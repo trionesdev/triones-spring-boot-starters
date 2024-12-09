39f3d0c23994e710b8e368b5cfe54590a04ac3d2
落雁沙 <fengxiaotx@163.com>
2024-12-09 21:56:25 +0800

    feat: add cache facade implement

--- triones-spring-boot-starter/src/main/java/com/trionesdev/boot/cache/autoconfigure/InfinispanCacheFacadeConfiguration.java
+++ triones-spring-boot-starter/src/main/java/com/trionesdev/boot/cache/autoconfigure/InfinispanCacheFacadeConfiguration.java
@@ -1,11 +1,14 @@
 package com.trionesdev.boot.cache.autoconfigure;
 
+import com.trionesdev.spring.cache.CacheFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
+@ConditionalOnMissingBean({CacheFacade.class})
 @Conditional({CacheFacadeCondition.class})
 public class InfinispanCacheFacadeConfiguration {
 }
--- triones-spring-boot-starter/src/main/java/com/trionesdev/boot/cache/autoconfigure/RedisCacheFacadeConfiguration.java
+++ triones-spring-boot-starter/src/main/java/com/trionesdev/boot/cache/autoconfigure/RedisCacheFacadeConfiguration.java
@@ -4,8 +4,10 @@ import com.trionesdev.spring.cache.CacheFacade;
 import com.trionesdev.spring.cache.RedisCacheFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -14,6 +16,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @RequiredArgsConstructor
 @Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
 @ConditionalOnClass({RedisTemplate.class})
 @ConditionalOnMissingBean({CacheFacade.class})
 @Conditional({CacheFacadeCondition.class})
