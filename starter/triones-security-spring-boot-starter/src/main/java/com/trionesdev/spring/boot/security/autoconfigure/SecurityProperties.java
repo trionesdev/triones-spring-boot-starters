134abbfbe07284866fa17887e2c6e8390efb0243
bane.shi <fengxiaotx@163.com>
2025-11-26 11:22:39 +0800

    feat: 更新工程结构，使dependencies 的版本跟工程保持一致

--- starter/triones-security-spring-boot-starter/src/main/java/com/trionesdev/spring/boot/security/autoconfigure/SecurityProperties.java
+++ starter/triones-security-spring-boot-starter/src/main/java/com/trionesdev/spring/boot/security/autoconfigure/SecurityProperties.java
@@ -1,7 +1,6 @@
 package com.trionesdev.spring.boot.security.autoconfigure;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
